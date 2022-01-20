e0ee9804a63f5409a3bfbfd0ac016663e4cba083
avick <avickmukh@gmail.com>
2022-01-20 15:19:07 +0530

    initial commit

---
+++ pages/fruits/[slug].tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FruitComponent() {
+  return (
+    <div>
+        Fruit
+    </div>
+  );
+}
---
+++ pages/fruits/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FruitsComponent() {
+  return (
+    <div>
+       Fruits Component
+    </div>
+  );
+}
